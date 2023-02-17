ef063db72e88ef1f614363b4a590c07bf050bad4
wm <1793584681@qq.com>
2023-02-17 16:42:30 +0800

    解决esLint

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from "react-router-dom"
 import { Provider } from "react-redux"
@@ -14,10 +14,10 @@ import theme from './assets/theme'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <HashRouter>
-          <App />
-        </HashRouter>
-      </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </ThemeProvider>
   </Provider>
 );
